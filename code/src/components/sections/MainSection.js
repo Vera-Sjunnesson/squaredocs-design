5e9810631de9dfeba80525d14b72324553b171bd
Vera Sjunnesson <vera.kastrup@gmail.com>
2023-11-24 12:03:43 +0100

    small fixes

--- code/src/components/sections/MainSection.js
+++ code/src/components/sections/MainSection.js
@@ -6,7 +6,7 @@ import InfoBlock from '../lib/InfoBlock';
 // Component for the main middle section
 const MainSection = () => {
   return (
-    <section className="w-5/6 flex flex-col items-center justify-start gap-20 pt-16 pb-20 px-20 lg:px-10 sm:px-5">
+    <section className="w-5/6 flex flex-col items-center justify-start gap-20 pt-16 pb-20 px-20 lg:px-10 sm:px-0">
       <Heading />
       <GuideButtons />
       <InfoBlock />
